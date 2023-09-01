acd61d41cfbb810df2a50be30469ba73b7342116
Prashant Gangwar <prashantgangwar45@gmail.com>
2023-09-02 00:49:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# To Do App
+A Simple 'To Do App' built with the help of **Node.js**, **Express.js**, **MongoDB(Mongoose)**, **EJS** and **CSS**.
+
+It helps you to list down your tasks and delete them when finished.
+
+## Hosted
+https://to-do-prashant-gangwar.herokuapp.com/
+
+## How to Run this App in your Local System ?
+Clone or Downlaod this repository in your local system.
+
+In the terminal, make sure you are inside the 'To-Do-App' folder. Type or Copy the following commands :-
+
+### 1) npm install
+ 
+It downloads all the dependencies used in the app.
+(Make sure you have MongoDB installed locally and running)
+
+
+### 2) nodemon index.js
+
+It runs our index.js file
+
+
+### 3) Now, ctrl + click on 'http://localhost:80' shown in terminal
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/to_do', {useNewUrlParser: true});
+
+const bodyParser = require("body-parser"); 
+app.use(bodyParser.urlencoded({extended: true}));//Syntax for using body-parser
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+const schemaName = new mongoose.Schema({
+    name: String
+});
+
+const modelName = mongoose.model('collectionName', schemaName);  
+
+app.get("/", function(req,res){
+    modelName.find(function (err, collectionnames){
+        if(err){console.log(err);}
+        else{
+            res.render("list", {addTask: collectionnames});
+            };
+      });
+});
+
+app.post("/", function(req,res){
+    let next = req.body.nextTask;//stores the text written by user in form using body-parser module
+    if(next !="") {
+        let document = new modelName ({
+        name: next
+        });
+        document.save();
+    };
+
+    let delTask = req.body.checked;
+    modelName.deleteOne({name: delTask}, function(err){
+            if(err){console.log(err);}
+            else{console.log("Task Deleted");}
+          });
+    res.redirect("/");//Redirects to the home route
+});
+
+const PORT = process.env.PORT ||80;
+app.listen(PORT, () => console.log(`To Do App listening at http://localhost:${PORT}`))
